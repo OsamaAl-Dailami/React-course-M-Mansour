07e80f53bc85f53f2ccc9237b1901f2fed3817c7
OsamaAl-Dailami <engos5544@gmail.com>
2025-02-22 02:42:19 +0300

    using true-false condition to control the flow

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
 function ListGroup() {
-  const items = ['An item', 'A second item', 'A third item', 'A fourth item'];
+  let items = ["An item", "A second item", "A third item", "A fourth item"];
+    items =[];
   return (
     <>
       <h1 className="mx-auto p-2">this is List Group</h1>
 
+      {items.length === 0 && "no found items"}
       <ul className="list-group">
-        {items.map(item=><li className="list-group-item" >{item}</li>)}
-      </ul>
+          {items.map((item, index) => (
+            <li key={index} className="list-group-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+
     </>
   );
 }
