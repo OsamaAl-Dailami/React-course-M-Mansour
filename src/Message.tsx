f485a77fa23e2cc5173352800ded994af32e1cad
osama sultan <engosamasultan@gmail.com>
2025-02-22 01:45:50 +0300

    Lesson 11 - Add bootstrap List group component to the app

---
+++ src/Message.tsx
@@ -0,0 +1,20 @@
+ function Message(){
+    const name = "os";
+
+    if(name){
+    return (
+        
+        <h1>Hello {name}</h1>
+
+    )
+
+    }else{
+        return (
+            <h1>Hello OS</h1>
+        )
+    }
+ }
+
+
+ export default Message;
+
